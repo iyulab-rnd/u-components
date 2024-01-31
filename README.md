980a788f152fdad37b9936525bbc5c7784dbc901
junehyung <jhcj910805@gmail.com>
2024-02-01 00:32:04 +0900

    update

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # u-components
-npm component library
+
+## Install
+```bash
+npm install @iyulab/u-components
+```
+
+## Usage
+```js
+import { } from "@iyulab/u-components";
+```
+
+## License
+MIT
+
+## Author
+- iyulab
+- [iyulab](https://www.iyulab.com)
---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+    build: {
+        lib: {
+            entry: 'src/index.ts', // 라이브러리의 진입점
+            name: 'MyLibrary', // UMD 빌드에서 사용될 전역 변수의 이름
+            fileName: (format) => `index.${format}.js`,
+            formats: ['cjs', 'es', 'umd'], // 빌드할 포맷
+        },
+        rollupOptions: {
+            // 외부화할 종속성을 지정
+            external: ['react', 'react-dom', 'react-router-dom', 'lit'],
+            output: {
+                // 전역 변수로 사용될 종속성 지정
+                globals: {
+                    react: 'React',
+                    'react-dom': 'ReactDOM',
+                    'react-router-dom': 'ReactRouterDOM',
+                    lit: 'Lit'
+                }
+            }
+        }
+    },
+    plugins: [
+        dts()
+    ]
+});
